15184472fca47cd5da74180280dc48476e522157
Digital Alicorn <digital_alicorn@outlook.com>
2021-09-22 17:56:56 +0200

    Submit Implementation.

---
+++ FSharp.Kata.Yahtzee/FSharp.Kata.Yahtzee.Tests/YahtzeeTests.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.FSharp;
+using FluentAssertions;
+
+namespace FSharp.Kata.Yahtzee.Tests
+{
+    [TestClass]
+    public class YahtzeeTests
+    {
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 1)]
+        [DataRow(1, 1, 3, 4, 5, 2)]
+        [DataRow(1, 1, 3, 1, 5, 3)]
+        [DataRow(6, 2, 3, 4, 5, 0)]
+        public void OnesAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Ones(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 2)]
+        [DataRow(2, 2, 3, 4, 5, 4)]
+        [DataRow(2, 2, 3, 2, 5, 6)]
+        [DataRow(1, 6, 3, 4, 5, 0)]
+        public void TwosAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Twos(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 3)]
+        [DataRow(1, 2, 3, 3, 5, 6)]
+        [DataRow(3, 2, 3, 4, 3, 9)]
+        [DataRow(1, 2, 6, 4, 5, 0)]
+        public void ThreesAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Threes(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 4)]
+        [DataRow(1, 2, 3, 4, 4, 8)]
+        [DataRow(4, 2, 4, 4, 5, 12)]
+        [DataRow(1, 2, 3, 6, 5, 0)]
+        public void FoursAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Fours(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 5)]
+        [DataRow(1, 2, 3, 5, 5, 10)]
+        [DataRow(5, 2, 3, 5, 5, 15)]
+        [DataRow(1, 2, 3, 4, 6, 0)]
+        public void FivesAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Fives(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(6, 2, 3, 4, 5, 6)]
+        [DataRow(6, 6, 3, 4, 5, 12)]
+        [DataRow(6, 6, 3, 6, 6, 24)]
+        [DataRow(1, 2, 3, 4, 5, 0)]
+        public void SixesAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Sixes(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 4, 4, 6, 6, 12)]
+        [DataRow(5, 2, 3, 4, 5, 10)]
+        [DataRow(6, 6, 3, 6, 6, 12)]
+        [DataRow(1, 2, 3, 4, 5, 0)]
+        public void HighestPairIsSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Pair(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 4, 4, 6, 6, 20)]
+        [DataRow(1, 1, 2, 3, 3, 8)]
+        [DataRow(6, 6, 3, 6, 6, 24)]
+        [DataRow(1, 2, 3, 5, 5, 0)]
+        public void TwoPairsAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.TwoPairs(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1, 1, 1, 6, 3)]
+        [DataRow(1, 2, 2, 2, 3, 6)]
+        [DataRow(4, 6, 4, 6, 4, 12)]
+        [DataRow(1, 2, 3, 4, 5, 0)]
+        public void ThreeOfAKindAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.ThreeOfAKind(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1, 1, 1, 6, 4)]
+        [DataRow(1, 2, 2, 2, 2, 8)]
+        [DataRow(4, 6, 4, 6, 4, 0)]
+        public void FourOfAKindAreSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.FourOfAKind(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 15)]
+        [DataRow(2, 3, 4, 5, 6, 0)]
+        public void SmallStraightIsSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.SmallStraight(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(2, 3, 4, 5, 6, 20)]
+        [DataRow(3, 4, 5, 6, 1, 0)]
+        public void LargeStraightIsSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.LargeStraight(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(4, 4, 4, 3, 3, 18)]
+        [DataRow(2, 2, 5, 5, 5, 19)]
+        [DataRow(4, 4, 4, 4, 4, 0)]
+        public void FullHouseIsSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.FullHouse(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1, 1, 1, 1, 50)]
+        [DataRow(1, 1, 1, 1, 2, 0)]
+        public void YahtzeeIsEvaluatedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Yahtzee(input).Should().Be(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 15)]
+        [DataRow(1, 1, 3, 4, 5, 14)]
+        [DataRow(1, 1, 3, 1, 5, 11)]
+        [DataRow(6, 2, 3, 4, 5, 20)]
+        public void ChanceIsSummedCorrectly(int x1, int x2, int x3, int x4, int x5, int result)
+        {
+            var input = Microsoft.FSharp.Collections.ListModule.OfSeq(new[] { x1, x2, x3, x4, x5 });
+            Yahtzee.Chance(input).Should().Be(result);
+        }
+    }
+}
---
+++ FSharp.Kata.Yahtzee/FSharp.Kata.Yahtzee/Yahtzee.fs
@@ -0,0 +1,66 @@
+﻿namespace FSharp.Kata.Yahtzee
+
+module Yahtzee =
+
+    let Ones diceRoll = List.sumBy(fun x -> if x = 1 then x else 0) diceRoll
+    
+    let Twos diceRoll = List.sumBy(fun x -> if x = 2 then x else 0) diceRoll
+    
+    let Threes diceRoll = List.sumBy(fun x -> if x = 3 then x else 0) diceRoll
+    
+    let Fours diceRoll = List.sumBy(fun x -> if x = 4 then x else 0) diceRoll
+    
+    let Fives diceRoll = List.sumBy(fun x -> if x = 5 then x else 0) diceRoll
+    
+    let Sixes diceRoll = List.sumBy(fun x -> if x = 6 then x else 0) diceRoll
+
+    let Pair diceRoll = 
+        match List.sortDescending diceRoll with 
+        | [a; b; _; _; _] when a = b -> a * 2
+        | [_; a; b; _; _] when a = b -> a * 2
+        | [_; _; a; b; _] when a = b -> a * 2
+        | [_; _; _; a; b] when a = b -> a * 2
+        | _ -> 0
+    
+    let TwoPairs diceRoll = 
+        match List.sortDescending diceRoll with
+        | [a; b; c; d; _] when a = b && c = d -> a * 2 + c * 2
+        | [a; b; _; c; d] when a = b && c = d -> a * 2 + c * 2
+        | [_; a; b; c; d] when a = b && c = d -> a * 2 + c * 2
+        | _ -> 0
+
+    let ThreeOfAKind diceRoll = 
+        match List.sortDescending diceRoll with 
+        | [a; b; c; _; _] when a = b && b = c -> a * 3
+        | [_; a; b; c; _] when a = b && b = c -> a * 3
+        | [_; _; a; b; c] when a = b && b = c -> a * 3
+        | _ -> 0
+
+    let FourOfAKind diceRoll = 
+        match List.sortDescending diceRoll with 
+        | [a; b; c; d; _] when a = b && b = c && c = d -> a * 4
+        | [_; a; b; c; d] when a = b && b = c && c = d -> a * 4
+        | _ -> 0
+
+    let SmallStraight diceRoll =
+        match diceRoll with
+        | [1; 2; 3; 4; 5] -> 15
+        | _ -> 0
+
+    let LargeStraight diceRoll = 
+        match diceRoll with
+        | [2; 3; 4; 5; 6] -> 20
+        | _ -> 0
+
+    let FullHouse diceRoll =
+        match List.sortDescending diceRoll with
+        | [a; b; c; d; e] when a = b && b <> c && c = d && d = e -> a * 2 + c * 3
+        | [a; b; c; d; e] when a = b && b = c && c <> d && d = e -> a * 3 + d * 2
+        | _ -> 0
+
+    let Yahtzee diceRoll = 
+        match List.sortDescending diceRoll with
+        | [a; b; c; d; e] when a = b && b = c && c = d && d = e -> 50
+        | _ -> 0
+
+    let Chance (diceRoll : int list) = List.sum diceRoll
